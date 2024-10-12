6ee0dc0763de2bec731e6a8ba92b60ed736f6c43
Temel Gunaydin <tgunaydin@gmail.com>
2024-10-12 13:07:02 +0300

    Initial Project files.Nothings changed yet

---
+++ CombineDemo/CombineDemo/CombineDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineDemoApp.swift
+//  CombineDemo
+//
+//  Created by temel gunaydin on 12.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct CombineDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
